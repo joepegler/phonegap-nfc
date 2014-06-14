702faa183ab8a7ec4cf6e8834905b32cd604391e
joepegler <jopegler@hotmail.com>
2014-06-14 15:10:04 +0100

    moved files around

--- www/phonegap-nfc.js
+++ www/phonegap-nfc.js
@@ -1,4 +1,232 @@
-/*jshint  bitwise: false, camelcase: false, quotmark: false, unused: vars */
+var Cription = (function Cription() {
+    var _privateVars = {
+        aes : { 
+            AES_Sbox : new Array(
+                99,124,119,123,242,107,111,197,48,1,103,43,254,215,171,
+                118,202,130,201,125,250,89,71,240,173,212,162,175,156,164,114,192,183,253,
+                147,38,54,63,247,204,52,165,229,241,113,216,49,21,4,199,35,195,24,150,5,154,
+                7,18,128,226,235,39,178,117,9,131,44,26,27,110,90,160,82,59,214,179,41,227,
+                47,132,83,209,0,237,32,252,177,91,106,203,190,57,74,76,88,207,208,239,170,
+                251,67,77,51,133,69,249,2,127,80,60,159,168,81,163,64,143,146,157,56,245,
+                188,182,218,33,16,255,243,210,205,12,19,236,95,151,68,23,196,167,126,61,
+                100,93,25,115,96,129,79,220,34,42,144,136,70,238,184,20,222,94,11,219,224,
+                50,58,10,73,6,36,92,194,211,172,98,145,149,228,121,231,200,55,109,141,213,
+                78,169,108,86,244,234,101,122,174,8,186,120,37,46,28,166,180,198,232,221,
+                116,31,75,189,139,138,112,62,181,102,72,3,246,14,97,53,87,185,134,193,29,
+                158,225,248,152,17,105,217,142,148,155,30,135,233,206,85,40,223,140,161,
+                137,13,191,230,66,104,65,153,45,15,176,84,187,22
+            ),
+            AES_ShiftRowTab : new Array(0,5,10,15,4,9,14,3,8,13,2,7,12,1,6,11),
+            AES_Init:function () {
+                this.AES_Sbox_Inv = new Array(256);
+                for(var i = 0; i < 256; i++){
+                    this.AES_Sbox_Inv[this.AES_Sbox[i]] = i;
+                }
+                this.AES_ShiftRowTab_Inv = new Array(16);
+                for(var i = 0; i < 16; i++){
+                    this.AES_ShiftRowTab_Inv[this.AES_ShiftRowTab[i]] = i;
+                }
+                this.AES_xtime = new Array(256);
+                for(var i = 0; i < 128; i++) {
+                    this.AES_xtime[i] = i << 1;
+                    this.AES_xtime[128 + i] = (i << 1) ^ 0x1b;
+                }
+            },
+            AES_Done:function () {
+                delete this.AES_Sbox_Inv;
+                delete this.AES_ShiftRowTab_Inv;
+                delete this.AES_xtime;
+            }, 
+            AES_ExpandKey:function (key) {
+                var kl = key.length, ks, Rcon = 1;
+                switch (kl) {
+                    case 16: 
+                        ks = 16 * (10 + 1); 
+                    break;
+                    case 24: 
+                        ks = 16 * (12 + 1); 
+                    break;
+                    case 32: 
+                        ks = 16 * (14 + 1); 
+                    break;
+                    default: 
+                        console.log("AES_ExpandKey: Only key lengths of 16, 24 or 32 bytes allowed!");
+                }
+                for(var i = kl; i < ks; i += 4) {
+                    var temp = key.slice(i - 4, i);
+                    if (i % kl == 0) {
+                        temp = new Array(this.AES_Sbox[temp[1]] ^ Rcon, this.AES_Sbox[temp[2]], this.AES_Sbox[temp[3]], this.AES_Sbox[temp[0]]); 
+                        if ((Rcon <<= 1) >= 256){
+                            Rcon ^= 0x11b;
+                        }
+                    }
+                    else if ((kl > 24) && (i % kl == 16)){
+                        temp = new Array(this.AES_Sbox[temp[0]], this.AES_Sbox[temp[1]], this.AES_Sbox[temp[2]], this.AES_Sbox[temp[3]]);
+                    }
+                    for(var j = 0; j < 4; j++){
+                        key[i + j] = key[i + j - kl] ^ temp[j];
+                    }
+                }
+            },
+            AES_Encrypt:function (block, key) {
+                var l = key.length;
+                this.AES_AddRoundKey(block, key.slice(0, 16));
+                for(var i = 16; i < l - 16; i += 16) {
+                    this.AES_SubBytes(block, this.AES_Sbox);
+                    this.AES_ShiftRows(block,this.AES_ShiftRowTab);
+                    this.AES_MixColumns(block);
+                    this.AES_AddRoundKey(block, key.slice(i, i + 16));
+                }
+                this.AES_SubBytes(block, this.AES_Sbox);
+                this.AES_ShiftRows(block, this.AES_ShiftRowTab);
+                this.AES_AddRoundKey(block, key.slice(i, l));
+            },
+            AES_Decrypt : function (block, key) {
+                var l = key.length;
+                this.AES_AddRoundKey(block, key.slice(l - 16, l));
+                this.AES_ShiftRows(block, this.AES_ShiftRowTab_Inv);
+                this.AES_SubBytes(block, this.AES_Sbox_Inv);
+                for(var i = l - 32; i >= 16; i -= 16) {
+                    this.AES_AddRoundKey(block, key.slice(i, i + 16));
+                    this.AES_MixColumns_Inv(block);
+                    this.AES_ShiftRows(block, this.AES_ShiftRowTab_Inv);
+                    this.AES_SubBytes(block, this.AES_Sbox_Inv);
+                }
+                this.AES_AddRoundKey(block, key.slice(0, 16));
+            },
+            AES_SubBytes : function (state, sbox) {
+                for(var i = 0; i < 16; i++){
+                    state[i] = sbox[state[i]];  
+                }
+            },
+            AES_AddRoundKey : function (state, rkey) {
+                for(var i = 0; i < 16; i++){
+                    state[i] ^= rkey[i];
+                }
+            }, 
+            AES_ShiftRows : function (state, shifttab) {
+                var h = new Array().concat(state);
+                for(var i = 0; i < 16; i++){
+                    state[i] = h[shifttab[i]];
+                }
+            },
+            AES_MixColumns : function (state) {
+                for(var i = 0; i < 16; i += 4) {
+                    var s0 = state[i + 0], s1 = state[i + 1];
+                    var s2 = state[i + 2], s3 = state[i + 3];
+                    var h = s0 ^ s1 ^ s2 ^ s3;
+                    state[i + 0] ^= h ^ this.AES_xtime[s0 ^ s1];
+                    state[i + 1] ^= h ^ this.AES_xtime[s1 ^ s2];
+                    state[i + 2] ^= h ^ this.AES_xtime[s2 ^ s3];
+                    state[i + 3] ^= h ^ this.AES_xtime[s3 ^ s0];
+                }
+            },
+            AES_MixColumns_Inv : function (state) {
+                for(var i = 0; i < 16; i += 4) {
+                    var s0 = state[i + 0], s1 = state[i + 1];
+                    var s2 = state[i + 2], s3 = state[i + 3];
+                    var h = s0 ^ s1 ^ s2 ^ s3;
+                    var xh = this.AES_xtime[h];
+                    var h1 = this.AES_xtime[this.AES_xtime[xh ^ s0 ^ s2]] ^ h;
+                    var h2 = this.AES_xtime[this.AES_xtime[xh ^ s1 ^ s3]] ^ h;
+                    state[i + 0] ^= h1 ^ this.AES_xtime[s0 ^ s1];
+                    state[i + 1] ^= h2 ^ this.AES_xtime[s1 ^ s2];
+                    state[i + 2] ^= h1 ^ this.AES_xtime[s2 ^ s3];
+                    state[i + 3] ^= h2 ^ this.AES_xtime[s3 ^ s0];
+                }
+            }
+        },
+        validate : {
+            err :   "",
+            isNotEmpty : function (value){
+                if(typeof value === 'undefined'||value==""){
+                    err+='\nA value you have entered is empty: '+value;
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            },
+            isValidKeyLength : function (value){
+                if(value.length==16||value.length==24||value.length==32){
+                    return true;
+                }
+                err+="\nInvalid key length: "+value.length+": "+value;
+                return false;
+            },
+            isAlphaNumeric : function (value){
+                if( /^[a-z0-9]+$/i.test(value)){
+                    return true;
+                }
+                err+="\nInvalid key, not alphanumeric: "+value;
+                return false;
+            },
+            isValid : function (test,data){
+                err="***INVALID DATA***";
+                var valid = false;
+                switch(test){
+                    case "secret":
+                        if (_privateVars.validate.isNotEmpty(data)){
+                            valid=true;
+                        }
+                    break;
+                    case "key":
+                        if (_privateVars.validate.isNotEmpty(data) && _privateVars.validate.isValidKeyLength(data) && _privateVars.validate.isAlphaNumeric(data)){
+                            valid=true;
+                        }
+                    break;
+                }
+                if (!valid){
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            }
+        },
+    };
+    return function CriptionConstructor() {
+        var _this = this; //Cache the `this` keyword
+        _this.encrypt = function(options){
+            if(!_privateVars.validate.isValid("key",options.key)){//If there is no key
+                if(_privateVars.validate.isValid("key",localStorage.getItem('key'))){//if there is a valid key in storage
+                    options.key=localStorage.getItem('key');//set the key to localStorage key
+                }
+            }
+            if(options.store && _privateVars.validate.isValid("key",options.key)&& _privateVars.validate.isValid("key",localStorage.getItem('key'))){//If you want to store it and you have a valid key and localStorage.key isnt already set...
+                localStorage.setItem('key', options.key);//set localStorage.key to the key you have 
+            }    
+            if(_privateVars.validate.isValid("secret",options.secret) && _privateVars.validate.isValid("key",options.key)){//if you have a valid secret and key then encrypt...
+                //options.secret=util.stringToBytes(options.secret);//DELETE THIS IF WE WE ALREADY HAVE BYTES...                
+                options.key=util.stringToBytes(options.key);//change the key to bytes
+                _privateVars.aes.AES_Init();//start
+                _privateVars.aes.AES_ExpandKey(options.key);//expand the key 
+                _privateVars.aes.AES_Encrypt(options.secret, options.key);//encrypt
+                _privateVars.aes.AES_Done();//finish
+                return options.secret; //should be a byte array
+            }
+        };
+        _this.decrypt=function(options){
+            if(!options.key||options.key==""||options.key==='undefined'){//If no key was provided
+                if( _privateVars.validate.isValid("key",localStorage.getItem('key')) ){//if there is a valid key in storage
+                    options.key = localStorage.getItem('key');//set the key to the one in storage
+                }
+            }
+            if(_privateVars.validate.isValid("secret",options.secret) && _privateVars.validate.isValid("key",options.key)){
+                options.key=util.stringToBytes(options.key);
+                _privateVars.aes.AES_Init();//start
+                _privateVars.aes.AES_ExpandKey(options.key);//expand the key
+                _privateVars.aes.AES_Decrypt(options.secret, options.key);//decrypt
+                _privateVars.aes.AES_Done();//finish
+                return options.secret;//should be a byte array
+            }
+        };
+    };
+}());
+var cription = new Cription();
+
+
+/*jshint  bitwise: false, camelcase: false, quotemark: false, unused: vars */
 /*global cordova, console */
 "use strict";
 
@@ -382,6 +610,38 @@ var nfc = {
         cordova.exec(win, fail, "NfcPlugin", "registerNdef", []);
     },
 
+    addAesNdefListener: function (callback, win, fail, myKey) {
+        function cb(nfcEvent){
+            var ndefMessage = nfcEvent.tag.ndefMessage;
+            for(i=0;i<ndefMessage.length;i++){
+                for(j=0;j<ndefMessage[i].payload.length;j++){
+                    if((ndefMessage[i].payload[j]<0)){
+                        ndefMessage[i].payload[j] += 256;
+                    }
+                }
+                cription.decrypt({secret:nfcEvent.tag.ndefMessage[i].payload, key:myKey});
+                for(j=0;j<ndefMessage[i].payload.length;j++){
+                    if(ndefMessage[i].payload[j]==0){
+                        ndefMessage[i].payload.splice(j,16-j);
+                    }
+                }
+            }
+            callback(nfcEvent);
+        };
+        document.addEventListener("ndef", cb, false);
+        cordova.exec(win, fail, "NfcPlugin", "registerNdef", []);
+    },
+
+    aesWrite: function (ndefMessage, win, fail, myKey) {
+        for(i=0;i<ndefMessage.length;i++){
+            for(j=ndefMessage[i].payload.length;j<16;j++){
+                ndefMessage[i].payload.push(0);
+            }
+            var result = cription.encrypt({secret:ndefMessage[i].payload, key:myKey});                    
+        }
+        cordova.exec(win, fail, "NfcPlugin", "writeTag", [ndefMessage]);
+    },
+
     addNdefFormatableListener: function (callback, win, fail) {
         document.addEventListener("ndef-formatable", callback, false);
         cordova.exec(win, fail, "NfcPlugin", "registerNdefFormatable", []);
@@ -400,7 +660,6 @@ var nfc = {
     },
 
     handover: function (uris, win, fail) {
-        // if we get a single URI, wrap it in an array
         if (!Array.isArray(uris)) {
             uris = [ uris ];
         }
